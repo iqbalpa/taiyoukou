944f1d5fb688e2f67d23f3b711b6559ab429d6e6
iqbalpa <iqbalpahlevi07@gmail.com>
2025-12-05 18:08:16 +0900

    update hatsuden

--- config/constant.py
+++ config/constant.py
@@ -1,29 +1,33 @@
 HATSUDEN_INFO = [
   {
     "plant_name": "ＡＯ電力津市安濃町太陽光発電所",
-    "romaji_name": "Tsushi",
+    "romaji_name": "AO-Denryoku-Tsu-Anou",
     "latitude": 34.7739640171019,
-    "longitude": 136.467498739582
+    "longitude": 136.467498739582,
+    "plant_id": "XAmqrLLKr9ak"
   },
   {
     "plant_name": "ＡＯ電力四日市市水沢町太陽光発電所",
-    "romaji_name": "Yokkaichi",
+    "romaji_name": "AO-Denryoku-Yokkaichi-Mizusawa",
     "latitude": 34.976332,
-    "longitude": 136.49064
+    "longitude": 136.49064,
+    "plant_id": "6n1MrxoRQaox"
   },
   {
     "plant_name": "ケーヨーデイツー姉崎店太陽光発電設備",
-    "romaji_name": "Anesaki",
+    "romaji_name": "Keiyo-D2-Anegasaki",
     "latitude": 35.4904,
-    "longitude": 140.0447
+    "longitude": 140.0447,
+    "plant_id": "axmXQRm7W4lN"
   },
   {
     "plant_name": "根室市花咲港245",
-    "romaji_name": "Nemuro",
+    "romaji_name": "Nemuro-Hanasaki",
     "latitude": 43.2946,
-    "longitude": 145.5834
+    "longitude": 145.5834,
+    "plant_id": "D9bmW3VxEYqd"
   }
-] 
+]
 
 OPENMETEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast"
 
