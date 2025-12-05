30206c04c11dce0455e01cfda945af8e1243951c
iqbalpa <iqbalpahlevi07@gmail.com>
2025-12-05 18:06:50 +0900

    script done

---
+++ config/constant.py
@@ -0,0 +1,35 @@
+HATSUDEN_INFO = [
+  {
+    "plant_name": "ＡＯ電力津市安濃町太陽光発電所",
+    "romaji_name": "Tsushi",
+    "latitude": 34.7739640171019,
+    "longitude": 136.467498739582
+  },
+  {
+    "plant_name": "ＡＯ電力四日市市水沢町太陽光発電所",
+    "romaji_name": "Yokkaichi",
+    "latitude": 34.976332,
+    "longitude": 136.49064
+  },
+  {
+    "plant_name": "ケーヨーデイツー姉崎店太陽光発電設備",
+    "romaji_name": "Anesaki",
+    "latitude": 35.4904,
+    "longitude": 140.0447
+  },
+  {
+    "plant_name": "根室市花咲港245",
+    "romaji_name": "Nemuro",
+    "latitude": 43.2946,
+    "longitude": 145.5834
+  }
+] 
+
+OPENMETEO_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+
+JMA_MODELS = ["jma_msm"]
+ECMWF_MODELS = ["ecmwf_ifs", "ecmwf_ifs025", "ecmwf_aifs025_single"]
+MODELS = {
+    "JMA": JMA_MODELS,
+    "ECMWF": ECMWF_MODELS
+}
---
+++ retriever.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+from config.constant import OPENMETEO_URL, HATSUDEN_INFO, MODELS
+
+def _fetch_data(latitude, longitude, models):
+	cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+	openmeteo = openmeteo_requests.Client(session = retry_session)
+
+	params = {
+		"latitude": {latitude},
+		"longitude": {longitude},
+		"start_date": "2023-12-31",
+		"end_date": "2025-01-01",
+		"hourly": "shortwave_radiation",
+		"models": models,
+		"timezone": "Asia/Tokyo",
+	}
+
+	responses = openmeteo.weather_api(OPENMETEO_URL, params=params)
+
+	for response in responses:
+		hourly = response.Hourly()
+		hourly_shortwave_radiation = hourly.Variables(0).ValuesAsNumpy()
+		
+		hourly_data = {"date": pd.date_range(
+			start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+			end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+			freq = pd.Timedelta(seconds = hourly.Interval()),
+			inclusive = "left"
+		)}
+		
+		hourly_data["shortwave_solar_radiation"] = hourly_shortwave_radiation
+		hourly_dataframe = pd.DataFrame(data=hourly_data)
+		return hourly_dataframe
+
+
+def get_data():
+	results = {}
+	for model_name, models in MODELS.items():
+		temp = []
+		for plant in HATSUDEN_INFO:
+			latitude = plant["latitude"]
+			longitude = plant["longitude"]
+			romaji_name = plant["romaji_name"]
+			plant_name = plant["plant_name"]
+			
+			hourly_dataframe = _fetch_data(latitude, longitude, models)
+			temp.append({
+				"romaji_name": romaji_name,
+				"plant_name": plant_name,
+				"dataframe": hourly_dataframe
+			})
+
+		for item in temp:
+			item["dataframe"]["plant_name"] = item["plant_name"]
+			item["dataframe"]["romaji_name"] = item["romaji_name"]
+
+		combined_dataframe = pd.concat([item["dataframe"] for item in temp], ignore_index=True)
+		combined_dataframe = combined_dataframe[
+			(combined_dataframe["date"] >= "2024-01-01") & (combined_dataframe["date"] < "2025-01-01")
+		].reset_index(drop=True)
+
+		results[model_name] = combined_dataframe
+
+	return results
+
+if __name__ == "__main__":
+	data = get_data()
+	for k, v in data.items():
+		print(f"Model: {k}")
+		print(v)
+		v.to_csv(f"data/{k.lower()}_data_2024.csv", index=False)
